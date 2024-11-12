cf25c7c90e8b6760d10c8bd610e943187066d5ce
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-11-12 23:29:45 +0700

    add: lua folders

---
+++ init.lua
@@ -0,0 +1,8 @@
+
+-----------------------------------------------------
+--- REQUIRES 
+-----------------------------------------------------
+require("globals/keymaps")    -- Nạp file lua/keymaps.lua
+require("plugins/packer")     -- File plugins cho packer
+-- Hiển thị số dòng
+vim.opt.number = true   
